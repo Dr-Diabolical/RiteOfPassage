69443967f45a213219a8dd6a001b518d0eedf8d8
Daniel Downes <danieldownescentennial@gmail.com>
2021-08-19 08:40:39 -0700

    Added loot table for sapphire block and creative tab
    Sapphire blocks now drop a sapphire block upon breaking with a iron pickaxe or higher, and the items in the mod are now in their own creative tab

--- src/main/java/com/drdiabolical/riteofpassage/registry/ModItems.java
+++ src/main/java/com/drdiabolical/riteofpassage/registry/ModItems.java
@@ -3,16 +3,15 @@ package com.drdiabolical.riteofpassage.registry;
 import com.drdiabolical.riteofpassage.RiteOfPassage;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
     // Items
-    public static final Item SAPPHIRE = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+    public static final Item SAPPHIRE = new Item(new Item.Settings().group(RiteOfPassage.ITEM_GROUP));
 
     // Block Items
-    public static final BlockItem SAPPHIRE_BLOCK = new BlockItem(ModBlocks.SAPPHIRE_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS));
+    public static final BlockItem SAPPHIRE_BLOCK = new BlockItem(ModBlocks.SAPPHIRE_BLOCK, new Item.Settings().group(RiteOfPassage.ITEM_GROUP));
 
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier(RiteOfPassage.MOD_ID, "sapphire"), SAPPHIRE);
